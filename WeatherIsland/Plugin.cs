6d4500f95861405b6c7d103f8a584d9e8081cf20
WRD1145 <1640884306@qq.com>
2026-07-11 18:26:27 +0800

    代码搬家

---
+++ WeatherIsland/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Reflection;
+using ClassIsland.Core;
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Extensions.Registry;
+using ClassIsland.Shared.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WeatherIsland.Models;
+using WeatherIsland.Views.SettingsPages;
+
+namespace WeatherIsland;
+
+[PluginEntrance]
+public class Plugin : PluginBase
+{
+    public static Plugin? Instance { get; private set; }
+
+    public Settings Settings { get; set; } = new();
+
+    public override void Initialize(HostBuilderContext context, IServiceCollection services)
+    {
+        
+        services.AddSettingsPage<WeatherIslandSettingsPage>();
+        Instance = this;
+    
+        var configPath = Path.Combine(PluginConfigFolder, "Settings.json");
+        Settings = ConfigureFileHelper.LoadConfig<Settings>(configPath);
+
+        Settings.PropertyChanged += (_, _) =>
+        {
+            ConfigureFileHelper.SaveConfig(configPath, Settings);
+        };
+        var locationId = (string)((dynamic)AppBase.Current).Settings.CityId;
+        locationId = locationId.Split(':')[1];
+        for (int i = 0; i <= 3; i++)
+        {
+            Console.WriteLine("地区代码"+locationId);
+        }
+        
+    }
+}
